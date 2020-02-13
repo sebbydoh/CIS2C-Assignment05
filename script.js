3e23a206f105a26977af46a3224fd24a45a75abb
Encinia <sebastienencinia@my.whccd.edu>
2020-02-12 19:46:05 -0800

    Completed question 1 and 2

---
+++ script.js
@@ -0,0 +1,48 @@
+// 1. Write a range function that takes two arguments, start and end, and returns an array
+// containing all the numbers from start up to (and including) end.
+// Example output:
+// console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function range(start, end){
+
+    let numbers = [];
+
+    for(let s = start; s <= end; s++){
+        numbers.push(s);
+    } 
+    return numbers;
+}
+
+console.log(range(1, 7));
+
+
+// 2. Next, write a sum function that takes an array of numbers and returns the sum of
+// these numbers. Run the example program and see whether it does indeed return 55.
+// Example output:
+// console.log(sum(range(1, 10))); // returns 55
+
+function sum(values){
+
+    let sumAmount = 0;
+
+    for(let c = 0; c <= values.length  - 1; c++){
+        sumAmount += values[c];
+    }
+    return sumAmount;
+}
+
+console.log(sum(range(1,5)));
+
+// 3. Arrays have a reverse method that changes the array by inverting the order in which
+// its elements appear. For this exercise, write two functions, reverseArray and
+// reverseArrayInPlace. The first, reverseArray, takes an array as an argument and
+// produces a new array that has the same elements in the inverse order. The second,
+// reverseArrayInPlace, does what the built-in reverse method does: it modifies the array
+// given as an argument by reversing its elements. Neither may use the standard reverse
+// method.
+// Example output for reverseArray:
+// console.log(reverseArray(["A", "B", "C"])); // outputs ["C", "B", "A"];
+// Example output for reverseArrayInPlace:
+// let arrayValue = [1, 2, 3, 4, 5];
+// reverseArrayInPlace(arrayValue);
+// console.log(arrayValue); // outputs [5, 4, 3, 2, 1]
